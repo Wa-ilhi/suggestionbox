22866869f6afad9cc38ad42a24e1e3820f0451df
Wa-ilhi <rlynebanks@gmail.com>
2025-08-07 12:49:14 +0800

    renewed

--- config.example.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {   
-  SUPABASE_URL: 'YOUR_SUPABASE_URL_HERE', // Replace with your actual Supabase URL
-  SUPABASE_KEY: 'YOUR_SUPABASE_KEY_HERE', // Replace with your actual Supabase key
-
-};
-export { CONFIG };
---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {   
+  SUPABASE_URL: 'https://xotjeiftthajvmbfzyeh.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvdGplaWZ0dGhhanZtYmZ6eWVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxODkxOTksImV4cCI6MjA2OTc2NTE5OX0.yGIZwFkZ5qw7Reu3OJleHw-dye4GmxZ1eNJZQIeFvUQ',
+
+};
+export { CONFIG };
